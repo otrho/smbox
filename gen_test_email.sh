2be95c1c3482a7d8377d2c5322a8bfafe92190e8
Toby Hutton <toby@grusly.com>
2021-07-24 19:18:54 +1000

    Improve the gen_test_email.sh script.
    In particular use ${USER} as the recipient.

--- gen_test_email.sh
+++ gen_test_email.sh
@@ -1,4 +1,17 @@
 #!/usr/bin/env bash
 
-curl -s https://loripsum.net/api/plaintext | fold -s | /usr/bin/mail -s "test mail $(date '+%d/%m %H:%M:%S')" toby
+check_bin() {
+  if [[ -z "$(type -p "$1")" ]] ; then
+    echo "error: $2 is required."
+    exit 1
+  fi
+}
+
+check_bin curl "'curl'"
+check_bin mail "a BSD-like 'mail'"
+
+curl -s https://loripsum.net/api/plaintext | \
+  fold -s | \
+  /usr/bin/mail -s "test mail $(date '+%d/%m %H:%M:%S')" ${USER}
+
 echo Done.
