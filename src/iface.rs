dda6a0801f9929ff1897261b01c93ef4423b6bf4
Toby Hutton <toby@grusly.com>
2021-08-04 17:37:21 +1000

    Load highlighting from a config file.
    Instead of hard coded regexes we can now load the highlighting from
    ${CONFIG_DIR}/smbox.toml.
    
    It is really basic, fragile code which will crap out if anything is
    slightly amiss, but it's a start.

--- src/iface.rs
+++ src/iface.rs
@@ -1,3 +1,5 @@
+use crate::highlight;
+
 // -------------------------------------------------------------------------------------------------
 
 #[derive(Debug)]
@@ -13,6 +15,7 @@ use termion::raw::IntoRawMode;
 pub fn run(
     lines: &Vec<String>,
     messages: &Vec<super::mbox::Message>,
+    highlighter: &mut highlight::Highlighter,
 ) -> std::io::Result<Vec<Action>> {
     let stdout = std::io::stdout().into_raw_mode()?;
     let stdout = termion::screen::AlternateScreen::from(stdout);
@@ -21,37 +24,8 @@ pub fn run(
     let mut terminal = tui::Terminal::new(tui::backend::TermionBackend::new(stdout))?;
     let mut iface = IfaceState::new(lines, messages);
 
-    let mut highlight_ctx = Highlighter {
-        ctx_matchers: vec![
-            HighlightContext {
-                ctx_re: regex::Regex::new(r"login failures:$").unwrap(),
-                ctx_id: "login failures".to_owned(),
-                matchers: vec![
-                    (
-                        regex::Regex::new(r"d......d").expect("Failed to compile RE."),
-                        107_u8,
-                    ),
-                    (regex::Regex::new(r" p[a-z]+ ").unwrap(), 167_u8),
-                    (
-                        regex::Regex::new(r"Bad protocol version.*(port)").unwrap(),
-                        140_u8,
-                    ),
-                    (regex::Regex::new(r"protocol version").unwrap(), 90_u8),
-                    (regex::Regex::new(r"Bad protocol").unwrap(), 100_u8),
-                ],
-            },
-            HighlightContext {
-                ctx_re: regex::Regex::new(r"denied packets:$").unwrap(),
-                ctx_id: "denied packets".to_owned(),
-                matchers: vec![(regex::Regex::new(r" p[a-z]+ ").unwrap(), 90_u8)],
-            },
-        ],
-        ctx_exit_re: Some(regex::Regex::new(r"^$").unwrap()),
-        ctx: None,
-    };
-
     iface.headers_state.select(Some(0));
-    iface.draw(&mut terminal, &mut highlight_ctx)?;
+    iface.draw(&mut terminal, highlighter)?;
 
     for inp_key in stdin.keys() {
         match inp_key? {
@@ -69,7 +43,7 @@ pub fn run(
             _ => (),
         }
 
-        iface.draw(&mut terminal, &mut highlight_ctx)?;
+        iface.draw(&mut terminal, highlighter)?;
     }
 
     Ok(iface
@@ -141,7 +115,7 @@ impl<'a> IfaceState<'a> {
     fn draw(
         &mut self,
         terminal: &mut IfaceTerminal,
-        highlighter: &mut Highlighter,
+        highlighter: &mut highlight::Highlighter,
     ) -> std::io::Result<()> {
         // A little lambda to remove the 'Title:' prefix and to pad/trunc to a fixed width.
         let prepare_field = |line: &str, width: usize| {
@@ -244,7 +218,7 @@ impl<'a> IfaceState<'a> {
 
 // -------------------------------------------------------------------------------------------------
 
-fn highlighted_line<'a>(highlighter: &mut Highlighter, line: &'a str) -> tui::text::Spans<'a> {
+fn highlighted_line<'a>(highlighter: &mut highlight::Highlighter, line: &'a str) -> tui::text::Spans<'a> {
     let mut spans = Vec::<tui::text::Span>::new();
     let mut save_span = |start, end, colour| match colour {
         None => {
@@ -274,83 +248,6 @@ fn highlighted_line<'a>(highlighter: &mut Highlighter, line: &'a str) -> tui::te
     save_span(final_idx, line.len(), final_colour);
 
     tui::text::Spans::from(spans)
-}
-
-// -------------------------------------------------------------------------------------------------
-
-struct Highlighter {
-    ctx_matchers: Vec<HighlightContext>,
-    ctx_exit_re: Option<regex::Regex>,
-    ctx: Option<String>,
-}
-
-struct HighlightContext {
-    ctx_re: regex::Regex,
-    ctx_id: String,
-    matchers: Vec<(regex::Regex, Colour256)>,
-}
-
-type Colour256 = u8;
-
-impl Highlighter {
-    fn get_highlights(&mut self, line: &str) -> Highlights {
-        if let Some(exit_re) = &self.ctx_exit_re {
-            if exit_re.is_match(line) {
-                self.ctx = None;
-            }
-        }
-
-        let new_ctx = self.ctx_matchers.iter().fold(None, |new_ctx, ctx_matcher| {
-            if ctx_matcher.ctx_re.is_match(line) {
-                Some(&ctx_matcher.ctx_id)
-            } else {
-                new_ctx
-            }
-        });
-        if new_ctx.is_some() {
-            self.ctx = new_ctx.map(|id| id.clone());
-            return Highlights {
-                highlights: Vec::new(),
-            };
-        }
-
-        let mut highlights = Vec::new();
-        for ctx_matcher in &self.ctx_matchers {
-            if self
-                .ctx
-                .as_ref()
-                .map_or(false, |cur_id| cur_id == &ctx_matcher.ctx_id)
-            {
-                for (re, colour) in &ctx_matcher.matchers {
-                    if let Some(caps) = re.captures(line) {
-                        let mtch = caps
-                            .get(if caps.len() == 1 { 0 } else { 1 })
-                            .expect("BUG! `caps` is guaranteed to have at least one match.");
-                        highlights.push(((mtch.start(), mtch.end()), *colour));
-                    }
-                }
-            }
-        }
-        Highlights { highlights }
-    }
-}
-
-struct Highlights {
-    highlights: Vec<((usize, usize), Colour256)>,
-}
-
-impl Highlights {
-    fn get_colour_at(&self, idx: usize) -> Option<Colour256> {
-        self.highlights
-            .iter()
-            .fold(None, |prev, ((start, end), colour)| {
-                if idx >= *start && idx < *end {
-                    Some(*colour)
-                } else {
-                    prev
-                }
-            })
-    }
 }
 
 // -------------------------------------------------------------------------------------------------
