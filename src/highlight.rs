b917b09d565fbfafc38ebca06a372da66b5893e1
Toby Hutton <toby@grusly.com>
2024-10-14 12:49:35 +1100

    Change the pattern matching context handling.
    The global context exit pattern is removed and now each context may have
    an optional local exit pattern.

--- src/highlight.rs
+++ src/highlight.rs
@@ -35,14 +35,16 @@ pub fn load_highlighter(toml_config: &str) -> Result<Highlighter, String> {
 
     match toml_value {
         toml::value::Value::Table(root_table) => {
-            let ctx_exit_re = root_table.get("exit_re").map(val_to_regex);
             let mut ctx_matchers = Vec::new();
             for (ctx_id, val) in root_table {
                 if let toml::value::Value::Table(section) = val {
-                    let ctx_re = section
+                    let ctx_enter_re = section
                         .get("re")
                         .map(val_to_regex)
                         .unwrap_or_else(|| panic!("Missing 're' value for '{}' context.", ctx_id));
+
+                    let ctx_exit_re = section.get("exit").map(val_to_regex);
+
                     let matchers =
                         section
                             .get("matchers")
@@ -50,18 +52,18 @@ pub fn load_highlighter(toml_config: &str) -> Result<Highlighter, String> {
                             .unwrap_or_else(|| {
                                 panic!("Missing 'matchers' value for '{}' context.", ctx_id)
                             });
+
                     ctx_matchers.push(HighlightContext {
-                        ctx_re,
-                        ctx_id,
+                        ctx_enter_re,
+                        ctx_exit_re,
                         matchers,
                     });
                 }
             }
 
             Ok(Highlighter {
-                ctx_exit_re,
                 ctx_matchers,
-                ctx: None,
+                cur_ctx: None,
             })
         }
         _ => Err("BUG! Config is not a table?".to_owned()),
@@ -74,61 +76,63 @@ type Colour256 = u8;
 
 pub struct Highlighter {
     ctx_matchers: Vec<HighlightContext>,
-    ctx_exit_re: Option<regex::Regex>,
-    ctx: Option<String>,
+    cur_ctx: Option<usize>,
 }
 
 struct HighlightContext {
-    ctx_re: regex::Regex,
-    ctx_id: String,
+    ctx_enter_re: regex::Regex,
+    ctx_exit_re: Option<regex::Regex>,
     matchers: Vec<(regex::Regex, Colour256)>,
 }
 
 impl Highlighter {
     pub fn get_highlights(&mut self, line: &str) -> Highlights {
-        if let Some(exit_re) = &self.ctx_exit_re {
-            if exit_re.is_match(line) {
-                self.ctx = None;
-            }
+        // First check if we're matching a new context.
+        if let Some(new_ctx_idx) = self
+            .ctx_matchers
+            .iter()
+            .enumerate()
+            .find_map(|(idx, ctx_matcher)| ctx_matcher.ctx_enter_re.is_match(line).then_some(idx))
+        {
+            // Set the new context and return empty highlights.
+            self.cur_ctx = Some(new_ctx_idx);
+            return Highlights::default();
         }
 
-        let new_ctx = self.ctx_matchers.iter().fold(None, |new_ctx, ctx_matcher| {
-            if ctx_matcher.ctx_re.is_match(line) {
-                Some(&ctx_matcher.ctx_id)
-            } else {
-                new_ctx
-            }
-        });
-        if new_ctx.is_some() {
-            self.ctx = new_ctx.cloned();
-            return Highlights {
-                highlights: Vec::new(),
-            };
-        }
+        // If we are matching a specific context then use it.
+        let mut highlights = Vec::default();
+        if let Some(cur_ctx_idx) = self.cur_ctx {
+            let ctx_matcher = &self.ctx_matchers[cur_ctx_idx];
 
-        let mut highlights = Vec::new();
-        for ctx_matcher in &self.ctx_matchers {
-            if self
-                .ctx
-                .as_ref()
-                .map_or(false, |cur_id| cur_id == &ctx_matcher.ctx_id)
+            // Firstly check if we're matching this context's exit pattern.
+            if ctx_matcher
+                .ctx_exit_re
+                .iter()
+                .any(|exit_re| exit_re.is_match(line))
             {
+                // Set the current context to none.
+                self.cur_ctx = None;
+            } else {
+                // Find any matches for this context.
                 for (re, colour) in &ctx_matcher.matchers {
                     if let Some(caps) = re.captures(line) {
                         let mtch = caps
                             .get(if caps.len() == 1 { 0 } else { 1 })
                             .expect("BUG! `caps` is guaranteed to have at least one match.");
+
                         highlights.push(((mtch.start(), mtch.end()), *colour));
                     }
                 }
             }
         }
+
         Highlights { highlights }
     }
 }
 
 // -------------------------------------------------------------------------------------------------
 
+#[derive(Default)]
 pub struct Highlights {
     highlights: Vec<((usize, usize), Colour256)>,
 }
